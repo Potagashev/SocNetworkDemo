d55c65043ba87fe098f9202e2bd42b9b068f62ea
Potagashev <alexpotagashev@gmail.com>
2022-11-07 01:58:58 +0700

    added postgres db

--- SocialNetworkDemo/config/settings.py
+++ SocialNetworkDemo/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)$5c-8+-a3nx9cbk8263%nvm_=tex^^glf^aw42k_s86uz=yjl
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['social-network-demo-uspeh.herokuapp.com','127.0.0.1']
 
 # Application definition
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
